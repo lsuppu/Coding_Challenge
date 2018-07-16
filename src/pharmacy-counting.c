8b678de6a501f7a10b7061e23d247391a1b26ac8
lsuppu <41200250+lsuppu@users.noreply.github.com>
2018-07-16 19:09:00 -0400

    Add files via upload

--- src/pharmacy-counting.c
+++ src/pharmacy-counting.c
@@ -1,11 +1,10 @@
-///bin/true;COMPILER_OPTIONS="-g -Wall -Wextra --std=c99 -O1 -fsanitize=address,undefined";THIS_FILE="$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")";OUT_FILE="/tmp/build-cache/$THIS_FILE";mkdir -p "$(dirname "$OUT_FILE")";test "$THIS_FILE" -ot "$OUT_FILE" || $(which clang || which gcc) $COMPILER_OPTIONS -xc "$THIS_FILE" -o "$OUT_FILE" || exit;exec "$OUT_FILE" "$@"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define MAX 200000                                  //Number of records in the input file
-#define INPUTFILE "itcont.txt.txt"      //Input file location
-#define OUTPUTFILE "top_cost_drug.txt"                     //Output file location
+#define MAX 2000                                          //Number of records in the input file
+#define INPUTFILE "itcont.txt"      //Input file location
+#define OUTPUTFILE "top_cost_drug.txt"                           //Output file location
 
 typedef struct rec{             //input STRUCT - Heterogenous Data Type
 	int id;                     //id
