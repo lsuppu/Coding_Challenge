178b49f22d75992c2844a705a0c17af812cbd2f3
lsuppu <41200250+lsuppu@users.noreply.github.com>
2018-07-16 18:37:46 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#gcc -o Pharmacy-counting pharmacy-counting.c
+#gcc -o Pharmacy-counting ./src/pharmacy-counting.c
 ./pharmacy-counting
-# ./src/pharmacy-counting.c ./input/itcont.txt ./output/top_cost_drug.txt
-
